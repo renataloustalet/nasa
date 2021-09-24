4ae51e9a0c9890ec673d538b8073c7f504f41445
renataloustalet <renataloustalet@hotmail.com>
2021-09-24 13:09:57 -0300

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,6 @@ import {
     APOD
 } from './constantes'
 
-const apiKey = process.env.REACT_APP_NASA_KEY;
-
 export function getApod(date) {
     return async function (dispatch) {
         const res = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=KAOnW0rZfgJYNSGq9H6Jj7XiIJJtdVhDfivrZKKF&date=${date}`)
